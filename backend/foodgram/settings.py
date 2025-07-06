af606b553a6b8a6d1d95b924dddad43ab8fd8500
Павел Дьяконов <vegence14@gmail.com>
2025-07-06 10:49:31 +0300

    test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
 DEBUG = True
-#DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+# DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', 'localhost,127.0.0.1').split(',')
 
