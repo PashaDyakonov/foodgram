6ccba9b24960ccc9b8c78b5a5793587aa09afe59
Павел Дьяконов <vegence14@gmail.com>
2025-07-15 23:28:21 +0300

    it4

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -41,7 +41,7 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'email', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     class Meta:
         ordering = ('username',)
