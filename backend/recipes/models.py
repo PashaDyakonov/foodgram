df6feb1788d51997d2e321113376a8bd79eab9cb
Павел Дьяконов <vegence14@gmail.com>
2025-07-19 01:33:44 +0300

    it6

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -170,7 +170,7 @@ class Recipe(models.Model):
     )
 
     class Meta:
-        ordering = ('slug',)
+        ordering = ('tag',)
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
 
