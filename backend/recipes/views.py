6b2d329278640b24a419704334a8cc5e6ad974f2
Павел Дьяконов <vegence14@gmail.com>
2025-07-19 00:14:09 +0300

    it6

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import redirect, get_object_or_404
-from recipes.models import Recipe
+from django.shortcuts import redirect
 
 
-def recipe_shortlink_redirect(request, pk):
+def recipe_shortlink_redirect(request, pk): 
     """Перенаправление с короткой ссылки на полный рецепт."""
-    get_object_or_404(Recipe, pk=pk)
     return redirect(f'/recipes/{pk}/')
