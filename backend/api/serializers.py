9aa022f4cc7d33efbc4f3f074d9dcb938b4fb68e
Павел Дьяконов <vegence14@gmail.com>
2025-07-20 02:29:12 +0300

    it7

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -149,14 +149,10 @@ class RecipeReadSerializer(serializers.ModelSerializer):
         )
 
     def check_user_status(self, obj, model):
-        user = self.context.get('request')
-        return (
-            user.user.is_authenticated
-            and model.objects.filter(
-                recipe=obj,
-                user=user.user
-            ).exists()
-        )
+        request = self.context.get('request')
+        if not request or not request.user.is_authenticated:
+            return False
+        return model.objects.filter(recipe=obj, user=request.user).exists()
 
     def get_is_favorited(self, obj):
         return self.check_user_status(obj, Favorite)
