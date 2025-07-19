e8bf389883274dbd617522086c94fc1d8372280e
Павел Дьяконов <vegence14@gmail.com>
2025-07-20 02:17:02 +0300

    it7

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -160,6 +160,7 @@ class Recipe(models.Model):
     )
     tags = models.ManyToManyField(
         Tag,
+        related_name='recipes',
         verbose_name='Теги',
         help_text='Выберите теги',
     )
