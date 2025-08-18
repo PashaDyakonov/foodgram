bdc0bd04e65c81587779b850ba17dfae69e0e621
Павел Дьяконов <vegence14@gmail.com>
2025-08-18 23:43:37 +0300

    it100

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import routers
 
 from api.views import (
@@ -20,4 +21,10 @@ router.register(r'users', UserViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
+]
+
+urlpatterns += [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
+         ),
 ]
